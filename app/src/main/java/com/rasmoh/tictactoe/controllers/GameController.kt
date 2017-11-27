0b3d64e8493db919a32a748bcc7713c8c59935cf
Jamie Eckman <jamie.eckman@gmail.com>
2017-11-26 16:27:55 -0800

    Basic game screen and navigation

--- app/src/main/java/com/rasmoh/tictactoe/controllers/GameController.kt
+++ app/src/main/java/com/rasmoh/tictactoe/controllers/GameController.kt
@@ -1,25 +1,20 @@
 package com.rasmoh.tictactoe.controllers
 
-import android.util.Log
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import butterknife.ButterKnife
-import butterknife.OnClick
 import butterknife.Unbinder
 import com.bluelinelabs.conductor.Controller
-import com.bluelinelabs.conductor.RouterTransaction
 import com.rasmoh.tictactoe.R
 
-private const val TAG = "HomeController"
+private const val TAG = "GameController"
 
-class HomeController : Controller() {
-
+class GameController : Controller() {
     private lateinit var unbinder: Unbinder
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
-        val view: View = inflater.inflate(R.layout.home, container, false)
+        val view: View = inflater.inflate(R.layout.game, container, false)
         unbinder = ButterKnife.bind(this, view)
         return view
     }
@@ -27,16 +22,5 @@ class HomeController : Controller() {
     override fun onDestroyView(view: View) {
         super.onDestroyView(view)
         unbinder.unbind()
-    }
-
-    @OnClick(R.id.new_game_button)
-    fun onNewGameClick() {
-        Log.d(TAG, "New game")
-        router.pushController(RouterTransaction.with(GameController()))
-    }
-
-    @OnClick(R.id.continue_game_button)
-    fun onContinueGameClick() {
-        Log.d(TAG, "Continue game")
     }
 }
